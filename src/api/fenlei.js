916f338eda58f2f5be6f2526b0bb5f0551c89b1f
gxb <550913664@qq.com>
2021-06-06 16:28:03 +0800

    2021.6.6拉取远程master后开始操作

--- src/api/fenlei.js
+++ src/api/fenlei.js
@@ -3,4 +3,4 @@ import { get } from '../utils/request'
 /*
   封装请求所有商品接口
 */
-export const reqAllproducts = () => get('/api/v1/products')
+export const reqAllproducts = (params) => get('/api/v1/products', params)
