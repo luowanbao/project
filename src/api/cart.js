3541a105a875afe7dc126935f88a2a04af24fad5
tianci.yang <ytc9199@163.com>
2021-06-07 16:10:48 +0800

    购物车增删改查，金额计算修改

--- src/api/cart.js
+++ src/api/cart.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-05 19:03:21
- * @LastEditTime: 2021-06-05 21:29:56
+ * @LastEditTime: 2021-06-07 11:19:42
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \project\src\api\cart.js
@@ -9,9 +9,15 @@
 //购物车
 
 import {
-    get
+    get,
+    post,
+    deletes
 } from '../utils/request'
-
+// 购物车列表
 export const reqGetCartList = () => get("/api/v1/shop_carts");
-
+// 商品列表
 export const reqGetProList = () => get("/api/v1/products");
+// 添加购物车
+export const reqAddCart = (params) => post("/api/v1/shop_carts", params);
+// 删除购物车
+export const reqDelPro = (params) => deletes("/api/v1/shop_carts/" + params);
