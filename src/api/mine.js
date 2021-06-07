9d5d0873cf629432ff6e5c3f84659f3e71a1c146
hanxueyi123 <1601264187@qq.com>
2021-06-07 22:59:01 +0800

    地址基本功能实现带点小bug

--- src/api/mine.js
+++ src/api/mine.js
@@ -1,2 +1,11 @@
-import { get } from '../utils/request'
+import { get, post ,deletes, put} from '../utils/request'
 export const DzlistApi = (params) => get('/api/v1/addresses', params)
+
+export const newDzApi =(data)=>post("/api/v1/addresses",data)
+
+export const deletApi = (id)=>deletes("/api/v1/addresses/"+id)
+
+// 单条地址详情
+export const oneddApi = (id)=>get("/api/v1/addresses/"+id)
+
+export const putApi = (id,params)=>put("/api/v1/addresses/"+id,params)
