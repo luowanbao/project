3a5580c3dcd47f60289812a13e88bf40921035b1
gxb <550913664@qq.com>
2021-06-08 10:40:37 +0800

    拉取远程dev最新代码

--- src/pages/myOrder/index.vue
+++ src/pages/myOrder/index.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>我的订单</div>
+  <div class="myOrder">
+    <!-- header部分 -->
+    <transition name="topbar">
+      <van-nav-bar
+        class="fenlei_top"
+        v-show="topbarShow"
+        title="商品订单"
+        left-arrow
+        :fixed="true"
+        :placeholder="true"
+        @click-left="onClickLeft"
+        @click-right="onClickRight"
+      >
+        <template #right>
+          <van-icon name="search" size="18" />
+        </template>
+      </van-nav-bar>
+    </transition>
+
+    <van-tabs v-model="activeName" :border="true" animated>
+      <!-- <van-tab v-for="index in 3" :key="index" :title="'选项 ' + index">
+        内容 {{ index }}
+      </van-tab> -->
+      <van-tab title="全部" name="allOrder">全部</van-tab>
+      <van-tab title="待付款" name="toBePaid">待付款</van-tab>
+      <van-tab title="待收货" name="toBeReceived">待收货</van-tab>
+    </van-tabs>
+  </div>
 </template>
 
 <script>
@@ -11,14 +38,25 @@ export default {
   components: {},
   data() {
     //这里存放数据,返回值为一个对象
-    return {};
+    return {
+      topbarShow: true,
+      activeName: 'allOrder',
+    };
   },
   //计算属性 依赖缓存,多对一(即多个影响一个),不支持异步
   computed: {},
   //监控data中的数据变化,不依赖缓存,一对多,支持异步
   watch: {},
   //方法集合
-  methods: {},
+  methods: {
+    onClickLeft() {
+      console.log('点击了返回按钮');
+    },
+
+    onClickRight() {
+      console.log('点击了搜索');
+    },
+  },
   //生命周期 - 创建完成（可以访问当前this实例）
   created() {},
   //生命周期 - 挂载完成（可以访问DOM元素）
@@ -33,4 +71,11 @@ export default {
 };
 </script>
 <style scoped>
+  /* topbar样式 */
+  .fenlei_top /deep/ .van-nav-bar  {
+    background: #F2F2F2;
+  }
+  .fenlei_top /deep/ .van-nav-bar .van-icon {
+    color: rgb(168, 159, 159);
+  }
 </style>
